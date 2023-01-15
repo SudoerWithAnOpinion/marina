4e34c323dad235bffd45c8440a0341cd6cbfc07f
Samuel Inman <samuel.q.inman@gmail.com>
2023-01-15 13:37:59 -0600

    feat(types): added UUID type

---
+++ src/types/UUID.d.ts
@@ -0,0 +1,5 @@
+type UUID = string & { isUUID: true }
+
+function isUUID(uuid: string): uuid is UUID {
+  return /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/.test(uuid);
+}
