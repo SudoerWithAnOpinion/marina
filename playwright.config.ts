567907ee48696af60391c397e35788c2483d58ab
Samuel Inman <samuel.q.inman@gmail.com>
2023-02-21 21:39:27 -0600

    chore(project): updated object to use base path added a post checkout hook for `pnpm install`

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,6 @@ const config: PlaywrightTestConfig = {
     command: 'npm run build && npm run preview',
       port: 4173
     },
-  testDir: 'tests'
 };
 
 export default config;
--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,9 +1,10 @@
 <script lang='ts'>
+  import { env } from '$env/dynamic/public';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
 </script>
 
 <Navbar let:hidden let:toggle class="bg-slate-500">
-  <NavBrand href="/">
+  <NavBrand href="{env.PUBLIC_BASE_PATH}">
     <img
       src="https://flowbite.com/docs/images/logo.svg"
       class="mr-3 h-6 sm:h-9"
@@ -16,6 +17,6 @@
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
     <!-- <NavLi href="/" active={true}>Home</NavLi> -->
-    <NavLi href="/printers">Printers</NavLi>
+    <NavLi href="{env.PUBLIC_BASE_PATH}printers">Printers</NavLi>
   </NavUl>
 </Navbar>
