3a128398fe077d23844e5b1dcbeb90cb8fb118cf
Samuel Inman <samuel.q.inman@gmail.com>
2023-02-23 15:53:45 -0600

    feat(app): add youch error handling
    Signed-off-by: Samuel Inman <samuel.q.inman@gmail.com>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,28 @@
 import { sequence } from '@sveltejs/kit/hooks';
-
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleServerError } from '@sveltejs/kit';
+import Youch from 'youch';
+import youchTerminal from 'youch-terminal';
 import { createTRPCHandle } from 'trpc-sveltekit';
+// Youch Error Handling
 
+export const handleError: HandleServerError = async ({error, event}) =>{
+  const youchError = await new Youch(error, {}).toJSON();
+  const options = {
+    displayShortPath: false,
+    prefix: ' ',
+    hideErrorTitle: false,
+    hideMessage: false,
+    displayMainFrameOnly: false,
+    framesMaxLimit: 3,
+  };
+  console.error(youchTerminal(youchError, options))
+}
+
+
+
+// tRPC
 export const trpcHandle: Handle = createTRPCHandle({
   router,
   createContext,
