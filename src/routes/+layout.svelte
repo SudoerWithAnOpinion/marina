8d265c037ef381f32cc1451b6e37cf048074f694
sinman <samuel.q.inman@gmail.com>
2023-02-28 04:44:30 +0000

    feat(auth): added auth functions to layout
    Signed-off-by: sinman <samuel.q.inman@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang='ts'>
-  import "../app.css";
+  import { page } from '$app/stores';
+  import { handleSession } from "@lucia-auth/sveltekit/client";
+  import '../app.css';
   import Navbar from '$components/Navbar/Navbar.svelte';
+  
+  handleSession(page);
 </script>
 
 <Navbar />
